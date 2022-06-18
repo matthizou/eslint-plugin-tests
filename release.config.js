f08f23ac8d1025bdff3be0c448758c2fa7fc4e77
Matt Izoulet <matt.izoulet@typeform.com>
2022-06-18 15:38:31 +0200

    feat: Semantic config

--- release.config.js
+++ release.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   branches: [
-    'main',
-    'next',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true }
-  ]
-}
+    "main",
+    "next",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
